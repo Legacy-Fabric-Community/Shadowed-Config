314a4a990511b44883181a2348a08f82c88b5ba5
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-19 09:56:08 +0530

    Nbt config
     Changes to be committed:
    	modified:   src/main/java/io/github/legacy_fabric_community/serialization/api/NbtConfigManager.java

--- src/main/java/io/github/legacy_fabric_community/serialization/api/NbtConfigManager.java
+++ src/main/java/io/github/legacy_fabric_community/serialization/api/NbtConfigManager.java
@@ -1,11 +1,17 @@
 package io.github.legacy_fabric_community.serialization.api;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 
 import javax.annotation.Nonnull;
 
+import io.github.legacy_fabric_community.serialization.nbt.NbtOps;
 import com.mojang.serialization.Codec;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
 
 public class NbtConfigManager<T> extends ConfigManager<T> {
     @Nonnull
@@ -18,17 +24,41 @@ public class NbtConfigManager<T> extends ConfigManager<T> {
 
     @Override
     public void serialize() throws IOException {
-
+        NbtIo.writeCompressed(
+                (CompoundTag) this.codec.encodeStart(
+                        NbtOps.INSTANCE,
+                        this.config
+                ).getOrThrow(false,
+                        PRINT_TO_STDERR
+                ),
+                new FileOutputStream(this.configPath.toFile())
+        );
     }
 
     @Override
     public void deserialize() throws IOException {
-
+        this.config = this.codec.decode(
+                NbtOps.INSTANCE,
+                NbtIo.readCompressed(
+                        new FileInputStream(this.configPath.toFile())
+                )
+        ).getOrThrow(
+                false,
+                PRINT_TO_STDERR
+        ).getFirst();
     }
 
     @Override
     protected void writeDefaultData() throws IOException {
-
+        NbtIo.writeCompressed(
+                (CompoundTag) this.codec.encodeStart(
+                        NbtOps.INSTANCE,
+                        this.defaultValue
+                ).getOrThrow(false,
+                        PRINT_TO_STDERR
+                ),
+                new FileOutputStream(this.configPath.toFile())
+        );
     }
 
     @Nonnull
