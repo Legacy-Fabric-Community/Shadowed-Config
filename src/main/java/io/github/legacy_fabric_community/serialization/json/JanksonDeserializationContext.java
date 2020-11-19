de0131207b514dd17b561b903f2dcb740d955a7c
SD <harrypotter.starwars.shrish@gmail.com>
2020-11-19 10:01:34 +0530

    Yeet contexts
     Changes to be committed:
    	deleted:    src/main/java/io/github/legacy_fabric_community/serialization/json/JanksonDeserializationContext.java
    	deleted:    src/main/java/io/github/legacy_fabric_community/serialization/json/JanksonSerializationContext.java
    	deleted:    src/main/java/io/github/legacy_fabric_community/serialization/json/JanksonSerializer.java

--- src/main/java/io/github/legacy_fabric_community/serialization/json/JanksonDeserializationContext.java
+++
@@ -1,9 +0,0 @@
-package io.github.legacy_fabric_community.serialization.json;
-
-import java.lang.reflect.Type;
-
-import blue.endless.jankson.JsonElement;
-
-public interface JanksonDeserializationContext {
-    <T> T deserialize(JsonElement json, Type type) throws IllegalStateException;
-}
